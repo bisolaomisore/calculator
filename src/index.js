2b1ed6d731d20a9cedc2928f9d8505e019c77b87
bbbruno <solaocodes@gmail.com>
2018-12-04 15:09:18 -0500

    Change color scheme of calulator buttons.

--- src/index.js
+++ src/index.js
@@ -14,33 +14,33 @@ class Calculator extends React.Component {
       <div id="calculator">
         <DisplayBox />
         <div id="row1">
-          <button className="circle">AC</button>
-          <button className="circle">+/-</button>
-          <button className="circle">%</button>
-          <button className="circle">/</button>
+          <button className="circle pink-white">AC</button>
+          <button className="circle pink-white">+/-</button>
+          <button className="circle pink-white">%</button>
+          <button className="circle pink-white">/</button>
         </div>
         <div id="row2">
-          <button className="circle">7</button>
-          <button className="circle">8</button>
-          <button className="circle">9</button>
-          <button className="circle">x</button>
+          <button className="circle white-red">7</button>
+          <button className="circle white-red">8</button>
+          <button className="circle white-red">9</button>
+          <button className="circle pink-white">x</button>
         </div>
         <div id="row3">
-          <button className="circle">4</button>
-          <button className="circle">5</button>
-          <button className="circle">6</button>
-          <button className="circle">+</button>
+          <button className="circle white-red">4</button>
+          <button className="circle white-red">5</button>
+          <button className="circle white-red">6</button>
+          <button className="circle pink-white">+</button>
         </div>
         <div id="row4">
-          <button className="circle">1</button>
-          <button className="circle">2</button>
-          <button className="circle">3</button>
-          <button className="circle">-</button>
+          <button className="circle white-red">1</button>
+          <button className="circle white-red">2</button>
+          <button className="circle white-red">3</button>
+          <button className="circle pink-white">-</button>
         </div>
         <div id="row5">
-          <button className="circle">0</button>
-          <button className="circle">.</button>
-          <button className="circle">=</button>
+          <button className="circle white-red">0</button>
+          <button className="circle white-red">.</button>
+          <button className="circle pink-white">=</button>
         </div>
       </div>
     );

5d544c174f3177d612e5aa47c19948d6926abad6
bbbruno <solaocodes@gmail.com>
2018-12-04 15:21:09 -0500

    Space calc btns horizontally.

--- src/index.js
+++ src/index.js
@@ -11,33 +11,33 @@ function DisplayBox(props) {
 class Calculator extends React.Component {
   render() {
     return (
-      <div id="calculator">
-        <DisplayBox />
-        <div id="row1">
+      <div id="calculator" >
+        <DisplayBox / >
+        <div id="row1" className="calc-row">
           <button className="btn circle pink-white">AC</button>
           <button className="btn circle pink-white">+/-</button>
           <button className="btn circle pink-white">%</button>
           <button className="btn circle pink-white">/</button>
         </div>
-        <div id="row2">
+        <div id="row2" className="calc-row">
           <button className="btn circle white-red">7</button>
           <button className="btn circle white-red">8</button>
           <button className="btn circle white-red">9</button>
           <button className="btn circle pink-white">x</button>
         </div>
-        <div id="row3">
+        <div id="row3" className="calc-row">
           <button className="btn circle white-red">4</button>
           <button className="btn circle white-red">5</button>
           <button className="btn circle white-red">6</button>
           <button className="btn circle pink-white">+</button>
         </div>
-        <div id="row4">
+        <div id="row4" className="calc-row">
           <button className="btn circle white-red">1</button>
           <button className="btn circle white-red">2</button>
           <button className="btn circle white-red">3</button>
           <button className="btn circle pink-white">-</button>
         </div>
-        <div id="row5">
+        <div id="row5" className="calc-row">
           <button className="btn circle white-red">0</button>
           <button className="btn circle white-red">.</button>
           <button className="btn circle pink-white">=</button>

f30a0a7db8e277611830fe22327be5a4891bca28
bbbruno <solaocodes@gmail.com>
2018-12-04 14:45:23 -0500

    Add all the calculator buttons and their values.

--- src/index.js
+++ src/index.js
@@ -13,6 +13,35 @@ class Calculator extends React.Component {
     return (
       <div id="calculator">
         <DisplayBox />
+        <div id="row1">
+          <button>AC</button>
+          <button>+/-</button>
+          <button>%</button>
+          <button>/</button>
+        </div>
+        <div id="row2">
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>x</button>
+        </div>
+        <div id="row3">
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>+</button>
+        </div>
+        <div id="row4">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>-</button>
+        </div>
+        <div id="row5">
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
       </div>
     );
   }
